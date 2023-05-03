f4cafb6e64d2b41f6c6e6ccc666b218933c64f9b
Yoon-Min <yoonmin0113@naver.com>
2023-05-04 00:31:58 +0900

    [LAYOUT] 장소 상세 페이지 제작

---
+++ app/src/main/java/com/example/travelfeeldog/presentation/location/LocationDetailFragment.kt
@@ -0,0 +1,22 @@
+package com.example.travelfeeldog.presentation.location
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
+import com.example.travelfeeldog.R
+import com.example.travelfeeldog.databinding.FragmentLocationDetailBinding
+import com.example.travelfeeldog.presentation.common.BaseFragment
+
+class LocationDetailFragment : BaseFragment<FragmentLocationDetailBinding>(R.layout.fragment_location_detail) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
