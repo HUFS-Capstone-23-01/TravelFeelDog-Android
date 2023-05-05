b5d3a36f732b5c2367f7a4bdfdb3bc991e384560
Yoon-Min <yoonmin0113@naver.com>
2023-05-05 23:58:16 +0900

    [ADD] 매니페스트에 서버 연결 관련 태그 추가

--- app/src/main/java/com/example/travelfeeldog/presentation/signin/SignInFragment.kt
+++ app/src/main/java/com/example/travelfeeldog/presentation/signin/SignInFragment.kt
@@ -1,18 +1,17 @@
 package com.example.travelfeeldog.presentation.signin
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import com.example.travelfeeldog.R
 import com.example.travelfeeldog.databinding.FragmentSignInBinding
 import com.example.travelfeeldog.presentation.common.BaseFragment
 import com.example.travelfeeldog.presentation.common.NavigationUtil.navigate
+import com.example.travelfeeldog.util.TestViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SignInFragment : BaseFragment<FragmentSignInBinding>(R.layout.fragment_sign_in) {
+
+    private val viewModel : TestViewModel by viewModel()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
