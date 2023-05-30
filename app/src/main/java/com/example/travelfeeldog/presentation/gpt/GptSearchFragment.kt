f7b98fcd8d04b06b72b1ea0762d0970707d2b0f3
Yoon-Min <yoonmin0113@naver.com>
2023-05-30 13:43:38 +0900

    [UI] GPT검색 페이지에 필요한 레이아웃 정의

---
+++ app/src/main/java/com/example/travelfeeldog/presentation/gpt/GptSearchFragment.kt
@@ -0,0 +1,20 @@
+package com.example.travelfeeldog.presentation.gpt
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.travelfeeldog.R
+import com.example.travelfeeldog.databinding.FragmentGptSearchBinding
+import com.example.travelfeeldog.presentation.common.BaseFragment
+
+class GptSearchFragment : BaseFragment<FragmentGptSearchBinding>(R.layout.fragment_gpt_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
